f88a8d71f117d14c068c547f381f8cff8f9131f6
fadilaaaa14 <nurfadhil1408@gmail.com>
2024-03-30 14:52:14 +0800

    date

--- class3.py
+++ class3.py
@@ -1,8 +1,8 @@
 class Work:
     def __init__(self,driver_name,plate_number,pickup_location,destination_location,distance,cosh):
-        self.driver_name = Name
-        self.plate_number = 12134 
-        self.pickup_location = (0,0)
+        self.driver_name = driver_name
+        self.plate_number = plate_number 
+        self.pickup_location = pickup_location
         self.destination_location = destination_location
         self.distance = distance 
         self.cosh = cosh 
@@ -11,7 +11,7 @@ class Work:
     def cekinfodriver(self):
         print(f"name : {self.name} \nplate_number : {self.plate_number} \npickup_location : {self.pickup_location} \ndestination_location : {self.destination_location} \ncosh : {self.cosh} \nrating : {self.rating}")
 
-        def
+    def
             
               
               
