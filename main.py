285ef4f7ebdccd455f8b5d36e96716bd50033247
MAlifDarulFirdaus <alifmadridista122@gmail.com>
2024-03-30 12:22:05 +0800

    main changes

--- main.py
+++ main.py
@@ -4,4 +4,11 @@ import class3
 
 
 class Sekolah:
-    def __init__ (self)
+    def __init__ (self,nilai, sks, kls):
+        self.nilai = nilai
+        self.sks = sks
+        self.kls = kls
+
+    def __str__(self):
+        return "Nilai: {}\nSKS: {}\nKelas: {}".format(self.nilai, self.sks, self.kls)
+    
