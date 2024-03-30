921b8d1bdccc41f61d2e08593426172edaf9dea5
fadilaaaa14 <nurfadhil1408@gmail.com>
2024-03-30 14:46:39 +0800

    dila

--- class3.py
+++ class3.py
@@ -1,15 +1,21 @@
-class work:
-    def __init__(self,driver_name,plate_number,pickup_location,destination_location):
+class Work:
+    def __init__(self,driver_name,plate_number,pickup_location,destination_location,distance,cosh):
         self.driver_name = Name
         self.plate_number = 12134 
         self.pickup_location = (0,0)
-        self.destination_location = 
-        self.distance = 
-        self.cosh = 
-        self.rating = 
+        self.destination_location = destination_location
+        self.distance = distance 
+        self.cosh = cosh 
+        self.rating = None
 
     def cekinfodriver(self):
-        for i in self.driver: 
+        print(f"name : {self.name} \nplate_number : {self.plate_number} \npickup_location : {self.pickup_location} \ndestination_location : {self.destination_location} \ncosh : {self.cosh} \nrating : {self.rating}")
+
+        def
+            
+              
+              
+            
 
                  
                  
