514f7aa63b3e1893a2564d2b93de81714e89b0d6
MAlifDarulFirdaus <alifmadridista122@gmail.com>
2024-03-30 15:46:43 +0800

    PErubahan 15.46

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ from class1 import User
 from class2 import Car
 from class3 import Work
 
-Jelita = User()
+Jelita = User("Jelita Nurwahida", 20, 80, 1000000, "Jelita@mail", 5, 10,40)
+
+Jelita.user_info
