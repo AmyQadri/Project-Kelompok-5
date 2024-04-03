2961380359f01cde167b6f607f98213bbe8ab361
MAlifDarulFirdaus <alifmadridista122@gmail.com>
2024-04-03 14:37:22 +0800

    Update Terbaru

---
+++ main.py
@@ -0,0 +1,59 @@
+from user import User
+from car import Car
+from application import WorkApplication
+
+CarMe = Car("Avanza","Toyota",35)
+Jelita = User("Jelita Nurwahida", 20,1000000, "Jelita@gmail.com")
+Driver = WorkApplication("Jelita",443301)
+Menu = 0
+
+
+while Menu != 10:
+    print("===============================================")
+    print("Selamat Datang! Pilih Menu Yang Ingin Dijalankan :")
+    print("1. Check Informasi Pengguna")
+    print("2. Check Informasi Mobil")
+    print("3. Check Informasi Driver")
+    print("4. Nyalakan Mesin Mobil")
+    print("5. Matikan Mesin Mobil")
+    print("6. Jalankan Aplikasi")
+    print("7. Jalankan Mobil")
+    print("8. Istirahat (Menambah Energy)")
+    print("9. Isi Bahan Bakar")
+    print("10. Exit")
+    Menu = int(input("Masukan Pilihan ::"))
+
+    if Menu == 1:
+        Jelita.UserInfo()
+    elif Menu == 2:
+        CarMe.CheckCarInfo()
+    elif Menu == 3:
+        Driver.CheckDriverInfo()
+    elif Menu == 4:
+        CarMe.StarEngine()
+    elif Menu == 5:
+        CarMe.StopEngine()
+    elif Menu == 6:
+        Jelita.Working(Driver,CarMe)
+    elif Menu == 7:
+        Jelita.RunCarForWorking(CarMe)
+    elif Menu == 8:
+        Jelita.PushEnergy()
+    elif Menu == 9:
+        CarMe.Refuel(Jelita)
+    elif Menu == 10:
+        print("Terima Kasih Atas Kehadiran Anda!")
+    else:
+        print("Invalid Menu!")
+        break
+
+
+
+'''
+KELOMPOK 5 ::
+
+M. ALIF DARUL FIRDAUS (236250005)
+MOH SURYA ALIF (236250021)
+JELITA NURWAHIDA LASADAM (236250016)
+NUR FADILAH (236250029)
+'''
